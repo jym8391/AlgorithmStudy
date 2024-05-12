7133500c776ce8041706a43f8bd7ddd6ff24db73
전영민 <65708226+jym8391@users.noreply.github.com>
2024-05-12 22:27:10 +0900

    Update BOJ 1504.cpp

--- 다익스트라/BOJ 1504. 특정한 최단 경로/BOJ 1504.cpp
+++ 다익스트라/BOJ 1504. 특정한 최단 경로/BOJ 1504.cpp
@@ -2,16 +2,15 @@
 #define INF 1e9+7
 #define pii pair<int,int>
 
+typedef long long ll;
+
 using namespace std;
 
-int N, M;
-int S,E; //시작 도시, 도착 도시
+int N, E;
+int v1, v2;//반드시 거쳐야하는 정점
 int dist[1001];
-int route[1001];
 vector<pii> v[1001];
 priority_queue<pii,vector<pii>, greater<pii> > pq; 
-//기본적으로 최대힙을 사용하는데, 최소힙을 사용하기 위해 greater를 이용했음
-vector<int> path; // 경로 기록 
 
 void dijkstra(int start) {
     for(int i = 1; i <= N; i++) {
@@ -21,8 +20,6 @@ void dijkstra(int start) {
     pq.push({0,start});
     while(!pq.empty()) {
         int cost = pq.top().first;
-        //만약 큐에 들어가는 값이 0보다 크다는것이 보장된다면 위처럼 복잡한 선언을 해주는대신 음수값을 넣어주는 방법도 있다.
-        //why? 
         int cur = pq.top().second;
         pq.pop();
         
@@ -34,43 +31,53 @@ void dijkstra(int start) {
             int next = v[cur][i].first;
             int ncost = v[cur][i].second;
             if(dist[next] > ncost + cost) {
-                route[next] = cur; //이전 노드 위치 기록
                 dist[next] = ncost + cost;
                 pq.push({dist[next], next});
             }
         }
     }
-    int T = E;
-    while(T) { //경로 역추적, 시작 노드로 가게 되면 시작 노드의 이전노드는 없으므로 종료됨
-        path.push_back(T);
-        T = route[T]; 
-    }
 }
 
 void input() {
     cin >> N;
-    cin >> M;
+    cin >> E;
     int s,e,t;
-    for(int i = 0; i < M; i++) {
+    for(int i = 0; i < E; i++) {
         cin >> s >> e >> t;
         v[s].push_back({e,t});
+        v[e].push_back({s,t});
     }
-    cin >> S >> E;
+    cin >> v1 >> v2;
 }
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
     
     input();
+    //첫 번째 경로 : 1 -> v1 -> v2 -> N
+    //두 번째 경로 : 1 -> v2 -> v1 -> N
+    //v1을 시작점으로 잡을 경우, v1->1, v1->v2, v1->N 
+    //v2를 시작점으로 잡을 경우, v2->1, v2->v1, v2->N 총 6가지가 나오는데 v1<->v2는 겹치기 때문에 5개만 구해줌
     
-    dijkstra(S);
+    ll v1_1, v1_v2, v1_N, v2_1, v2_N;
+    ll res1 = 0, res2 = 0, res = 0;
+    dijkstra(v1);
+    
+    v1_1 = dist[1];
+    v1_v2 = dist[v2];
+    v1_N = dist[N];
 
-    cout << dist[E] << "\n";
-    cout << path.size() << "\n";
-    for(int i=path.size()-1;i>=0;i--) {
-        cout << path[i] << " ";
-    }
-    cout << "\n";
+    dijkstra(v2);
+    
+    v2_1 = dist[1];
+    v2_N = dist[N];
+    
+    res1 = v1_1 + v1_v2 + v2_N;
+    res2 = v2_1 + v1_v2 + v1_N;
+    res = min(res1,res2);
+
+    if(res>=INF) cout << "-1\n";
+    else cout << res << "\n";
 
     return 0;
 }
